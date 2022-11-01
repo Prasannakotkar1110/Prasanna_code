d168dac2761a9778e98e63f91a638bc9080aa770
Prasannakotkar1110 <kotkarprasanna11@gmail.com>
2022-11-01 22:13:03 +0530

    Program_DBContaxt_implementation

---
+++ Prasanna_code/PrasannaCode.API/Data/Program_DbContaxt.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrasannaCode.API.model.Domain;
+
+namespace PrasannaCode.API.Data
+{
+    public class Program_DbContaxt : DbContext
+    {
+        public Program_DbContaxt(DbContextOptions<Program_DbContaxt> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Region>? Regions { get; set; }
+
+        public DbSet<Walk>? Walks { get; set; }
+
+        public DbSet<WalkDifficulty>? WalkDifficulty { get; set; }
+
+    }
+}
